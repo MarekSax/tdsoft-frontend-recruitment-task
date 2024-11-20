7aeac2c88a5419f963e4ef4f1433c4948184f62b
Marek <mareksax@wp.pl>
2024-11-20 19:25:57 +0100

    chore: add colors.ts with variables matching Figma design

---
+++ styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  blue: 'rgba(178, 208, 235, 1)',
+  green: 'rgba(135, 199, 64, 0.15)',
+  red: 'rgba(235, 87, 87, 0.15)',
+  grey1: 'rgba(51, 51, 51, 1)',
+  grey2: 'rgba(79, 79, 79, 1)',
+  grey3: 'rgba(130, 130, 130, 1)',
+  grey5: 'rgba(224, 224, 224, 1)',
+  grey6: 'rgba(238, 238, 238, 1)',
+};
