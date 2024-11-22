b2099d81cb8e36c04ec0af97d316295e8103bceb
Marek <mareksax@wp.pl>
2024-11-22 18:57:24 +0100

    chore: add padding to the page

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const AppContainer = styled.main`
   display: flex;
-  margin: 8px;
+  padding: 8px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
